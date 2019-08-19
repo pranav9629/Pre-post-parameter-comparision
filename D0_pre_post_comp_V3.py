1233e4c75e4060e9e92c0fabcfc45c8083d60ed9
Pranav <46915136+pranav9629@users.noreply.github.com>
2019-08-19 18:54:57 +0530

    Add files via upload

---
+++ D0_pre_post_comp_V3.py
@@ -0,0 +1,151 @@
+
+# pre.xlsx // post.xlsx // output.xlsx
+
+
+import openpyxl
+
+wb_pre = openpyxl.load_workbook('pre.xlsx')
+wb_post = openpyxl.load_workbook('post.xlsx')
+sheet_pre = wb_pre.get_sheet_by_name('All KPIs')
+sheet_post = wb_post.get_sheet_by_name('All KPIs')
+cell_list = []
+
+# getting unique cell list (pre)
+
+for x in range(2,sheet_pre.max_row+1):
+    for y in range(2,sheet_pre.max_row+1):
+        if sheet_pre.cell(row=y,column=1).value == sheet_pre.cell(row=x,column=1).value:
+            if sheet_pre.cell(row=y,column=1).value not in cell_list:
+                cell_list.append(sheet_pre.cell(row=y,column=1).value)
+            else:
+                continue
+        else:
+            continue
+
+# getting pre rrc total
+
+i = 0
+pre_rrc_sum = []
+for cell in cell_list:
+    for y in range(2,sheet_pre.max_row+1):
+        if sheet_pre.cell(row=y,column=1).value == cell:
+            i = i+sheet_pre.cell(row=y,column=6).value
+        else:
+            continue
+    pre_rrc_sum.append(i)
+    i = 0
+    
+# getting pre throughput total
+
+i = 0
+pre_thpt_sum = []
+n =0
+for cell in cell_list:
+    for y in range(2,sheet_pre.max_row+1):
+        if sheet_pre.cell(row=y,column=1).value == cell:
+            if bool(sheet_pre.cell(row=y,column=5).value) == False:
+                n = 'N.A'
+            else:
+                n = n+sheet_pre.cell(row=y,column=5).value
+        else:
+            continue
+    if n == 'N.A':
+        pre_thpt_sum.append(n)
+    else:
+        pre_thpt_sum.append(n/3)
+    i = 0
+    n = 0
+
+    
+postcell_list = []
+
+# getting unique cell list (post)
+
+for x in range(2,sheet_post.max_row+1):
+    for y in range(2,sheet_post.max_row+1):
+        if sheet_post.cell(row=y,column=1).value == sheet_post.cell(row=x,column=1).value:
+            if sheet_post.cell(row=y,column=1).value not in postcell_list:
+                postcell_list.append(sheet_post.cell(row=y,column=1).value)
+            else:
+                continue
+        else:
+            continue
+
+# getting post rrc total
+
+i = 0
+post_rrc_sum = []
+for cell in postcell_list:
+    for y in range(2,sheet_post.max_row+1):
+        if sheet_post.cell(row=y,column=1).value == cell:
+            i = i+sheet_post.cell(row=y,column=6).value
+        else:
+            continue
+    post_rrc_sum.append(i)
+    i = 0  
+    
+# getting post throughput total
+
+i = 0
+post_thpt_sum = []
+n = 0
+for cell in postcell_list:
+    for y in range(2,sheet_post.max_row+1):
+        if sheet_post.cell(row=y,column=1).value == cell:
+            if bool(sheet_post.cell(row=y,column=5).value) == False:
+                n = 'N.A'
+            else:
+                n = n+sheet_post.cell(row=y,column=5).value
+        else:
+            continue
+    if n == 'N.A':
+        post_thpt_sum.append(n)
+    else:
+        post_thpt_sum.append(n/3)
+    i = 0 
+    n = 0
+
+# pasting to excel
+
+wb_op = openpyxl.load_workbook('output.xlsx')
+preop = wb_op.get_sheet_by_name('pre')
+postop = wb_op.get_sheet_by_name('post')
+for x in range(len(cell_list)):
+    preop.cell(row=x+2,column=1).value = cell_list[x]
+    preop.cell(row=x+2,column=2).value = pre_rrc_sum[x]
+    preop.cell(row=x+2,column=3).value = pre_thpt_sum[x]
+for x in range(len(postcell_list)):
+    postop.cell(row=x+2,column=1).value = postcell_list[x]
+    postop.cell(row=x+2,column=2).value = post_rrc_sum[x]
+    postop.cell(row=x+2,column=3).value = post_thpt_sum[x]
+wb_op.save('output.xlsx')
+
+# comparision of pre and post outputs
+
+compop = wb_op.get_sheet_by_name('output')
+for x in range(len(cell_list)):
+    for y in range(len(postcell_list)):
+        if postcell_list[y] == cell_list[x]:
+            compop.cell(row=x+2,column=1).value = cell_list[x]
+            compop.cell(row=x+2,column=2).value = pre_rrc_sum[x]
+            compop.cell(row=x+2,column=3).value = post_rrc_sum[y]
+            compop.cell(row=x+2,column=4).value = pre_thpt_sum[x]
+            compop.cell(row=x+2,column=5).value = post_thpt_sum[y]
+        else:
+            continue
+wb_op.save('output.xlsx')
+
+for x in range(2,compop.max_row+1):
+    if compop.cell(row=x,column=2).value != 0:
+        compop.cell(row=x,column=6).value = (((compop.cell(row=x,column=3).value)-(compop.cell(row=x,column=2).value))/(compop.cell(row=x,column=2).value))*100
+    else:
+        compop.cell(row=x,column=6).value = 'No Pre RRC traffic'
+        
+for x in range(2,compop.max_row+1):
+    if compop.cell(row=x,column=4).value != 0 and compop.cell(row=x,column=4).value != 'N.A' and compop.cell(row=x,column=5).value != 0 and compop.cell(row=x,column=5).value != 'N.A':
+        compop.cell(row=x,column=7).value = (((compop.cell(row=x,column=5).value)-(compop.cell(row=x,column=4).value))/(compop.cell(row=x,column=4).value))*100
+    else:
+        compop.cell(row=x,column=7).value = 'No Pre DL Throughput'       
+        
+wb_op.save('output.xlsx')    
+    
